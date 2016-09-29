858261d80734376f2bb338e40062cba59bb45e79
AlejandroStevensonDuran <astevensond@utexas.edu>
2016-09-28 20:59:36 -0500

    Add files via upload

--- DFS_check.java
+++ DFS_check.java
@@ -8,15 +8,13 @@ import java.util.Iterator;
 
 public class DFS_check {
 
-//	String state;
 	String lastword; // the one to be compared to
 	String start_word;
 	String end_word;
 	ArrayList<String> curr_path;
 	int path_size = 0;
-//	ArrayList<String> result;
 	
-	DFS_check(ArrayList<String> path){  // constructos
+	DFS_check(ArrayList<String> path){  //constructors
 		this.curr_path = path;
 	}
 	
@@ -25,11 +23,8 @@ public class DFS_check {
 			this.start_word = start;
 			this.end_word = end;
 			this.lastword = lastword;
-			//this.curr_path = new ArrayList<String>();
 			this.curr_path = path;
 			this.path_size = size;
-			//this.result = result;
-		//	index = 0;
 		}
 
 	public static ArrayList<String> DFSwordladder(Set<String> dictionary, String start, String end){
@@ -37,34 +32,40 @@ public class DFS_check {
 		curr_path.add(start); // start path with first word
 		ArrayList<String> answer = new ArrayList<String>();
 		DFS_check firstDFS =new DFS_check(start, end, start, curr_path, 1);
-		ArrayList<String> result = getDFSladder(firstDFS, dictionary, answer);
-	
+		ArrayList<String> visited = new ArrayList<String>();
+
+		ArrayList<String> result = getDFSladder(firstDFS, dictionary, visited, answer);	
+		
 		return result;
 	}
 	
-	public static ArrayList<String> getDFSladder(DFS_check myDFS, Set<String> dictionary, ArrayList<String> answer){
+	public static ArrayList<String> getDFSladder(DFS_check myDFS, Set<String> dictionary, ArrayList<String> visited, ArrayList<String> answer){
+		//dictionary.remove(myDFS.lastword); // remove from dict
+		visited.add(myDFS.lastword); //adds to visited nodes list
 		if(myDFS.lastword.equals(myDFS.end_word)){ // means a ladder has been completed
-			System.out.println("kinda success");
 			answer = myDFS.curr_path;
 			return answer;
 		}
 		else{
 			Iterator<String> iterator = dictionary.iterator();
 			while(iterator.hasNext()){
-				String curr_word = iterator.next();
+				String curr_word = iterator.next();		
+				if(!(visited.contains(curr_word))){
 				
 				if(!myDFS.curr_path.contains(curr_word)){ // don't check if current word is already in path to prevent looping
 				if(word_checker(myDFS.lastword, curr_word)){ // check if it differs by one letter
 					myDFS.curr_path.add(curr_word);
 					DFS_check next_DFS = new DFS_check(myDFS.start_word,myDFS.end_word, curr_word, myDFS.curr_path, myDFS.path_size+1);
-					ArrayList<String> checky = getDFSladder(next_DFS, dictionary, answer); // check new word as new path
+					ArrayList<String> checky = getDFSladder(next_DFS, dictionary, visited, answer); // check new word as new path
 					if(checky != null){ // means answer has been updated so just leave
 						return checky;
 					}
 					//myDFS.path_size = myDFS.path_size - 1;
 					myDFS.curr_path.remove(myDFS.path_size);  // after check, remove from path
 					}
+				
 				}	
+			}
 			}
 			return null;
 		}
